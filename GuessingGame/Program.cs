8765fa4ed9af3f823e512a3e5c915e2b1dd263d0
Justin Sweeten <jtsweeten@gmail.com>
2022-11-21 09:53:34 -0600

    initial commit

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+void GuessingGame()
+{
+    pickANumber();
+};
+int theSecretNumber = 42;
+void pickANumber()
+{
+    Console.Write("Guess the number? (1-50):");
+    int userGuess = int.Parse(Console.ReadLine());
+    int i = 1;
+    while (userGuess != theSecretNumber && i < 4)
+    {
+
+
+        if (userGuess != theSecretNumber)
+        {
+            Console.WriteLine("Try again (1-50):");
+            userGuess = int.Parse(Console.ReadLine());
+
+        }
+
+
+
+        i++;
+    }
+    if (userGuess == theSecretNumber)
+    {
+        Console.WriteLine("You guessed it!");
+
+    }
+    else
+    {
+        Console.WriteLine("Sorry your wrong");
+
+    }
+}
+
+
+
+
+GuessingGame();
